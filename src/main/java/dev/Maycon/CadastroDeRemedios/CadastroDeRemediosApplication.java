1775fe2aff086a37015669f6b5dde0968f45b3b7
Maycon Do Carmo <maycongusmao.up@gmail.com>
2025-04-22 17:44:10 -0300

    first commit

---
+++ src/main/java/dev/Maycon/CadastroDeRemedios/CadastroDeRemediosApplication.java
@@ -0,0 +1,13 @@
+package dev.Maycon.CadastroDeRemedios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeRemediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeRemediosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Maycon/CadastroDeRemedios/CadastroDeRemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Maycon.CadastroDeRemedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeRemediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
