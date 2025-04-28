a3cf81a0b7b968b93e1886336a8d8dd99f51df95
Maycon Do Carmo <maycongusmao.up@gmail.com>
2025-04-28 17:46:09 -0300

    feat: Altera a rota do controller para listar remedios e injeta dependencias do SERVICE e REPOSITORY

--- src/main/java/dev/Maycon/CadastroDeRemedios/Remedios/RemedioController.java
+++ src/main/java/dev/Maycon/CadastroDeRemedios/Remedios/RemedioController.java
@@ -1,15 +1,52 @@
 package dev.Maycon.CadastroDeRemedios.Remedios;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class RemedioController {
 
+    private RemedioService remedioService;
+
+    public RemedioController(RemedioService remedioService) {
+        this.remedioService = remedioService;
+    }
+
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return  "Primeira mensagem nessa rota";
+    }
+
+    //Adicionar remedios (CREATE)
+    @PostMapping("/criar")
+    public String criarRemedio(){
+        return "Remedio Criado";
+    }
+
+    //Mostrar todos os remedios (READ)
+    @GetMapping("/listar")
+    public List<RemedioModel> listarRemedios() {
+        return remedioService.listarRemedios();
+    }
+
+
+    //Mostrar remedios por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarRemedioPorID(){
+        return "mostra todos remediosID";
+    }
+
+    //Alterar dados dos remedios (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorID(){
+        return "Alterar Ninja por id";
+    }
+
+    //Deletar remedios (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarRemedioPorID(){
+        return "deletar remedio por id";
     }
 }
--- src/main/java/dev/Maycon/CadastroDeRemedios/Remedios/RemedioService.java
+++ src/main/java/dev/Maycon/CadastroDeRemedios/Remedios/RemedioService.java
@@ -1,4 +1,22 @@
 package dev.Maycon.CadastroDeRemedios.Remedios;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RemedioService {
+
+    private RemedioRepository remedioRepository;
+
+    public RemedioService( RemedioRepository remedioRepository){
+        this.remedioRepository = remedioRepository;
+    }
+
+    //Listar todos remedios
+
+    public List<RemedioModel> listarRemedios(){
+        return remedioRepository.findAll();
+    }
+
 }
