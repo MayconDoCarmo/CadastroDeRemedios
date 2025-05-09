4e775fabf1fc532fd44a02ed652c1bd129514bfb
Maycon Do Carmo <Maycongusmao.up1@gmail.com>
2025-05-09 14:11:25 -0300

    feat: Cria DTOs da entidade Lote

--- src/main/java/dev/Maycon/CadastroDeRemedios/Lotes/dto/LoteResponseDTO.java
+++ src/main/java/dev/Maycon/CadastroDeRemedios/Lotes/dto/LoteResponseDTO.java
@@ -1,4 +1,22 @@
 package dev.Maycon.CadastroDeRemedios.Lotes.dto;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class LoteResponseDTO {
+
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    private String codigo;
+
+    private LocalDate dataDeFabricacao;
+
+    private LocalDate dataDeValidade;
 }
